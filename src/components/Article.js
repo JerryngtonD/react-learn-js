9963195377d62803af489db6b772c399fd4068ee
jerryngton <jerryngtond@gmail.com>
2018-07-04 23:25:23 +0300

    added reverse data flow

---
+++ src/components/Article.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {findDOMNode} from 'react-dom';
+import PropTypes from 'prop-types';
+import CommentList from './CommentList';
+import toggleOpen from '../decorators/toggleOpen';
+
+class Article extends Component {
+    static propTypes = {
+        article: PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            title: PropTypes.string.isRequired,
+            text: PropTypes.string
+        }).isRequired
+    };
+
+    render() {
+        const {article, isOpen, toggleOpen} = this.props;
+        return (
+            <div ref = {this.setContainerRef}>
+                <h3>{article.title}</h3>
+                <button onClick={toggleOpen}>
+                    {isOpen ? 'close' : 'open'}
+                </button>
+                {this.getBody()}
+            </div>
+        );
+    };
+
+    setContainerRef = ref => {
+        this.container = ref;
+        console.log('----', findDOMNode(ref));
+    };
+
+    getBody() {
+        const {article, isOpen} = this.props;
+        if(!isOpen) return null;
+        return (
+            <section>
+                {article.text}
+                <CommentList comments = {article.comments} ref={this.setContainerRef}/>
+            </section>
+        )
+    };
+}
+
+export default Article;
---
+++ src/components/ArticleList.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import Article from './Article';
+
+export default class ArticleList extends Component  {
+	state = {
+		openArticleId: null,
+		closed: true
+	};
+
+	render() {
+	    const articleElements = this.props.articles.map((article) =>
+	     	<li key = {article.id}>
+	    		<Article 
+	    			article = {article}
+	    			isOpen = {article.id === this.state.openArticleId && this.state.closed }
+	    			toggleOpen ={this.toggleOpenArticle(article.id)}
+	    		/>
+	    	</li>);
+	    	
+	    return (
+	        <ul>
+	            {articleElements}
+	        </ul>
+	    )
+	}
+
+	toggleOpenArticle = (openArticleId) => ev => {
+		this.setState({
+			openArticleId,
+			closed: !this.state.closed
+		})
+	}
+}
