88e8612b51b8aafb47e9582591cea8bdd5ed26d0
jerryngton <jerryngtond@gmail.com>
2018-07-06 09:07:09 +0300

    article list to decorator

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import Article from './Article';
 
-export default function ArticleList ({articles}) {
-    const articleElements = articles.map((article) => <li key = {article.id}><Article article = {article}/></li>)
-    return (
-        <ul>
-            {articleElements}
-        </ul>
-    )
+class ArticleList extends Component {
+
+    static propTypes = {
+        articles: PropTypes.array.isRequired,
+        openItemId: PropTypes.string,
+        toggleOpenItem: PropTypes.func.isRequired
+    };
+
+    render() {
+        const {articles, openItemId, toggleOpenItem}  = this.props;
+        const articleElements = articles.map((article) => <li key={article.id}>
+            <Article
+                article = {article}
+                isOpen = {article.id === openItemId}
+                toggleOpen = {toggleOpenItem(article.id)}
+            />
+        </li>);
+        return (
+            <ul>
+                {articleElements}
+            </ul>
+        )
+    }
 }
