c19de67ad56f9beff64db285cb22cac9fd2c2450
jerryngton <jerryngtond@gmail.com>
2018-07-11 12:32:52 +0300

    AC for filter events were added

--- src/AC/index.js
+++ src/AC/index.js
@@ -1,4 +1,4 @@
-import {DELETE_ARTICLE, INCREMENT} from '../constants';
+import {DELETE_ARTICLE, INCREMENT, CHANGE_DATE_RANGE, CHANGE_SELECTION} from '../constants';
 
 export function increment() {
     return {
@@ -14,3 +14,19 @@ export function deleteArticle (id) {
         }
     }
 }
+
+export function changeDateRange(dateRange) {
+    return {
+        type: CHANGE_DATE_RANGE,
+        payload: { dateRange }
+    }
+}
+
+export function changeSelection(selected) {
+    return {
+        type: CHANGE_SELECTION,
+        payload: { selected }
+    }
+}
+
+
