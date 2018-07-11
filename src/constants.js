f69882913928de979c7775676635764405a38e71
jerryngton <jerryngtond@gmail.com>
2018-07-11 12:34:01 +0300

    Сonctants for filter events -> select or dateRange

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,9 @@ export const DELETE_ARTICLE = 'DELETE_ARTICLE';
 
 export const INCREMENT = 'INCREMENT';
 
+export const CHANGE_DATE_RANGE = 'CHANGE_DATA_RANGE';
+
+export const CHANGE_SELECTION = 'CHANGE_SELECTION';
+
+
 
